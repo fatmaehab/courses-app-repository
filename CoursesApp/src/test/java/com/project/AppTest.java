ea66c59931bcc4e1d003567fb27d0cecec5babd3
fatmaehab <45612151+fatmaehab@users.noreply.github.com>
2019-05-23 10:14:17 +1000

    update test cases

--- CoursesApp/src/test/java/com/project/AppTest.java
+++ CoursesApp/src/test/java/com/project/AppTest.java
@@ -2,17 +2,17 @@ package com.project;
 
 //import static org.junit.Assert.*;
 
-import java.util.logging.*;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import com.project.services.CourseService;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//import java.util.logging.*;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//import com.project.services.CourseService;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class AppTest {
 
 //	@Test
